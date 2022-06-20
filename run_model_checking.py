bf1ae6e89c7758f6d557f7a81d8776f733f39179
Nick Shoemaker <nick@tug-swl-a194.tugraz.at>
2022-06-20 12:36:38 +0200

    Added parsing for all results when using the optimized files

--- run_model_checking.py
+++ run_model_checking.py
@@ -2,9 +2,10 @@ import pandas as pd
 import numpy as np
 import subprocess, copy
 from tqdm import tqdm
-prism_executable = "../../prism-4.7-linux64/bin/prism"
+prism_executable = "prism/prism/bin/prism"
 from matplotlib import pyplot as plt
 import random,json
+import re
 
 
 def sigma_for_bob(bob):
@@ -58,10 +59,13 @@ def check_one_state(initBob, initSigma, initGuard, initTime, openDoor):
                             check=True)
     output = result.stdout
     # print(output)
-    result_position = output.rfind("Result: ")
+    res_expr = re.compile("Result: ([\d.]+)")
+    results = res_expr.findall(output)
+    result_pmin, result_pmax, result_rmax = results
+    """result_position = output.rfind("Result: ")
     result_pmin = output[result_position:].split('Result: ')[1].split(' (')[0]
     result_pmax = 0.1 # to do
-    result_rmax = 0.1 # to do
+    result_rmax = 0.1 # to do"""
 
     props_file = f"{props_folder}/p_master_dtmc.props"
     result = subprocess.run(args=[prism_executable, str(prism_file_dtmc), str(props_file)],
@@ -70,9 +74,12 @@ def check_one_state(initBob, initSigma, initGuard, initTime, openDoor):
                             check=True)
     output = result.stdout
     # print(output)
-    result_position = output.rfind("Result: ")
+    res_expr = re.compile("Result: ([\d.]+)")
+    results = res_expr.findall(output)
+    result_pxi, result_rxi = results
+    """result_position = output.rfind("Result: ")
     result_pxi = output[result_position:].split('Result: ')[1].split(' (')[0]
-    result_rxi = 0.1 # todo
+    result_rxi = 0.1 # todo """
     ################
     ## End optimized
     ################
@@ -179,7 +186,6 @@ def main():
             auxdict = {'trace' : i, 'strat' : strat, 'rhoP' : auxdf.Pxi.sum()/auxdf.Pmax.sum(),
                        'rhoR' : auxdf.Rxi.sum()/auxdf.Rmax.sum()}
             df = df.append(auxdict, ignore_index=True)
-
     df.to_csv('data/raw_data.csv')
 
 if __name__ == "__main__":
